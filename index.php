66823b237d649e0d34c1625bc6582087fbe63864
Rap Esteva <rapunk27@gmail.com>
2017-01-11 17:31:38 +0800

    modified colors and added primary picture and greeting message

--- index.php
+++ index.php
@@ -1,17 +1,39 @@
-<!DOCTYPE html>
-<html>
-<head>
-	<title>Homepage</title>
-	  <meta charset="utf-8">
-    <meta http-equiv="x-ua-compatible" content="ie=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Foundation for Sites</title>
-    <link rel="stylesheet" href="css/foundation.css">
-    <link rel="stylesheet" href="css/app.css">
-    <link href="https://fonts.googleapis.com/css?family=Lato|Merriweather|Merriweather+Sans" rel="stylesheet">
+<?php include('head.php'); ?>
 
-</head>
-<body>
-		<h1>Hello there!</h1>
-</body>
-</html>
+<div class="top-bar custom-top-nav">
+    <div class="top-bar-left">
+    	<div class="top-bar-title small-push-3">
+    		<h3 class="name-title">Ralph Martyn Esteva</h3>
+    		<h6 class="position-text">Web Designer/Developer</h6>
+    	</div>
+    </div>
+    <div class="top-bar-right">
+    	<ul class="menu large-pull-3 small-push-3 nav-style">
+    		<li class="active"><a href="#">Home</a></li>
+    		<li><a href="#">Works</a></li>
+    		<li><a href="#">Resume</a></li>
+    		<li><a href="#">Contacts</a></li>
+    	</ul>
+    </div>
+</div>
+
+<div class="parallax-background">
+  <div class="intro-text">
+  	<div class="row">
+  		<div class="small-4 small-push-2 columns">
+  			<h1 class="greetings-text">Hello there!</h1>
+  			<p class="greetings-msg">My name is Rap. First of all, thank you for stopping by my page.
+  			I am a persuasive graduate of Computer Science in Jose Rizal University with a passion for coding and making music as well. </p>
+
+  		</div>
+  		<div class="small-8 columns">
+  				<img src="images/rapunk.jpg" class="circle-profile">
+  		</div>
+  	</div> 	
+    <p><i class="fi-arrow-down"></i></p>
+  </div>
+</div>
+
+
+
+<?php include('foot.php'); ?>
