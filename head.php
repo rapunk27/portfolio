5515f210fb2d480c416850795eca183c9734f5ea
Rap Esteva <rapunk27@gmail.com>
2017-01-11 17:30:51 +0800

    added foot and head files for a more organized folder structure

---
+++ head.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Homepage</title>
+	  <meta charset="utf-8">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/foundation.css">
+    <link rel="stylesheet" href="css/app.css">
+    <link rel="stylesheet" href="css/styles.css">
+<link href="https://fonts.googleapis.com/css?family=Lato|Merriweather|Merriweather+Sans|Playfair+Display+SC" rel="stylesheet">
+
+</head>
+<body>
