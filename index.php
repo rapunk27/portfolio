d4c250f5f4075b425bbe81e26811a8d88b3976a8
Rap Esteva <rapunk27@gmail.com>
2017-01-11 14:31:00 +0800

    initialized project

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Homepage</title>
+	  <meta charset="utf-8">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Foundation for Sites</title>
+    <link rel="stylesheet" href="css/foundation.css">
+    <link rel="stylesheet" href="css/app.css">
+    <link href="https://fonts.googleapis.com/css?family=Lato|Merriweather|Merriweather+Sans" rel="stylesheet">
+
+</head>
+<body>
+		<h1>Hello there!</h1>
+</body>
+</html>
