4f32d1c5dfd56fa713ffb9e74ebd502f92aa46f3
Rap Esteva <rapunk27@gmail.com>
2017-01-13 13:43:01 +0800

    renamed resume to work.php

---
+++ works.php
@@ -0,0 +1,13 @@
+<?php include('head.php'); ?>
+
+<div class="works-content">
+	
+	<a href="#" class="circle-default">
+
+		<div class="circle-work circle-systems"></div>
+		<p class="circle-work-text">Systems</p>
+	</a>
+</div>
+
+
+<?php include('foot.php'); ?>
