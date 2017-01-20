a6a8699bf18f6ce8992c67d68e9ddcb75dff9ce9
Rap Esteva <rapunk27@gmail.com>
2017-01-20 21:33:38 +0800

    added resume page

--- head.php
+++ head.php
@@ -25,7 +25,7 @@
     	<ul class="menu large-pull-3 small-push-3">
     		<li><a href="<?php echo $path; ?>index.php">Home</a></li>
     		<li><a href="<?php echo $path; ?>works.php">Works</a></li>
-    		<li><a href="resume.php">Resume</a></li>
+    		<li><a href="<?php echo $path; ?>resume.php">Resume</a></li>
     		<li><a href="#">Contacts</a></li>
     	</ul>
     </div>
---
+++ resume.php
@@ -0,0 +1,58 @@
+<?php 
+$path = ''; 
+$path2 = '';
+?>
+<?php include('head.php'); ?>
+<div class="parallax-action">
+	<div class="row resume-title">
+		<div class="columns">
+			<h1 class="text-center resume-title-size">My Resume Page</h1>
+		</div>
+	</div>
+</div>
+<div class="resume-section">
+
+	<div class="row">
+		<div class="small-4 columns">
+			<img class="resume-circle" src="images/rap_selfie.jpg">
+		</div>
+		<div class="small-8 columns">
+			<b class="resume-info-label">Nickname: </b> 
+			<p class="resume-info-ans">Rap</p>
+		</div>
+	</div>
+
+
+
+
+
+
+
+<!-- 	<div class="row">
+		<div class="small-6 columns">
+			<div class="card" style="width: 600px;">
+			  <div class="card-divider">
+			    <h4>I'm featured</h4>
+			  </div>
+			  <img src="images/work.jpg">
+			  <div class="card-section">
+			    <p>This card makes use of the card-divider element.</p>
+			  </div>
+			</div>
+		</div>
+
+		<div class="small-6 columns">
+			<div class="card" style="width: 600px;">
+			  <div class="card-divider">
+			    <h4>I'm featured</h4>
+			  </div>
+			  <img src="images/work.jpg">
+			  <div class="card-section">
+			    <p>This card makes use of the card-divider element.</p>
+			  </div>
+			</div>
+		</div>
+	</div> -->
+</div>
+
+<?php include('foot.php'); ?>
