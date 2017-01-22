4491a867a64064ca4595c4b1da1869dad43824dc
Rap Esteva <rapunk27@gmail.com>
2017-01-22 14:30:03 +0800

    finished resume page

--- resume.php
+++ resume.php
@@ -14,45 +14,127 @@ $path2 = '';
 
 	<div class="row">
 		<div class="small-4 columns">
-			<img class="resume-circle" src="images/rap_selfie.jpg">
+			<img class="resume-square" src="images/rap_selfie.jpg">
 		</div>
 		<div class="small-8 columns">
-			<b class="resume-info-label">Nickname: </b> 
-			<p class="resume-info-ans">Rap</p>
+			<div class="row text-center">
+				<b class="resume-info-label">Web Technologies learned: </b> 
+			</div>
+			<div class="row">
+				<div class="small-5 small-offset-1 columns resume-info-ans">
+					<ul>
+						<li>PHP</li>
+						<li>Laravel (Php framework)</li>
+						<li>Git (Version control)</li>
+						<li>Adobe Photoshop</li>
+						<li>HTML / HTML Email</li>
+						<li>MySql</li>
+						<li>HeidiSql (MySql Application)</li>
+						<li>XAMPP (Server Package)</li>
+					</ul>
+				</div>
+				<div class="small-5 small-offset-1 columns resume-info-ans">
+					<ul>
+						<li>CSS</li>
+						<li>Bootstrap / Foundation (CSS Frameworks)</li>
+						<li>Javascript / Jquery(Javascript library)</li>
+						<li>Filezilla (File Transfer Protocol</li>
+						<li>Sublime (Text Editor)</li>
+					</ul>
+				</div>
+			</div>
+			<div class="row">
+				<div class="small-12 columns text-center">
+					<a href="https://docs.google.com/document/d/1T9xusbBds1czVpNup_1GHnqh9exn_w_JP1uS-gVVXfM/edit?usp=sharing" class="large button warning" target="_blank"> 
+						<i class="fa fa-file-word-o"></i>
+						View Resume Document Here
+					</a>
+				</div>
+			</div>
 		</div>
 	</div>
+</div>
+<div class="parallax-work">
+	<div class="row resume-title">
+		<div class="columns">
+			<h1 class="text-center resume-title-size">Work Experience</h1>
+		</div>
+	</div>
+</div>
 
-
-
-
-
-
-
-<!-- 	<div class="row">
-		<div class="small-6 columns">
-			<div class="card" style="width: 600px;">
-			  <div class="card-divider">
-			    <h4>I'm featured</h4>
-			  </div>
-			  <img src="images/work.jpg">
-			  <div class="card-section">
-			    <p>This card makes use of the card-divider element.</p>
-			  </div>
+<div class="resume-work-section">
+<!-- Asiatravel -->
+	<div class="row">
+		<div class="small-8 small-offset-2 columns">
+			<div class="callout warning" style="overflow: hidden;">
+				<div class="resume-work-company">
+					<h4 class="resume-work-bold">
+						Asiatravel.com
+					</h4>
+					<p class="resume-work-para">Web Designer/Publisher - Dubai</p>
+				</div>
+				<div class="resume-work-date">
+					<p class="resume-work-bold">
+						June 15, 2016 - Present
+					</p>
+				</div>
 			</div>
 		</div>
-
-		<div class="small-6 columns">
-			<div class="card" style="width: 600px;">
-			  <div class="card-divider">
-			    <h4>I'm featured</h4>
-			  </div>
-			  <img src="images/work.jpg">
-			  <div class="card-section">
-			    <p>This card makes use of the card-divider element.</p>
-			  </div>
+	</div>
+<!-- PEP -->
+	<div class="row">
+		<div class="small-8 small-offset-2 columns">
+			<div class="callout warning" style="overflow: hidden;">
+				<div class="resume-work-company">
+					<h4 class="resume-work-bold">
+						Linc Academy - Ayala Education
+					</h4>
+					<p class="resume-work-para">Professional Employment Program</p>
+				</div>
+				<div class="resume-work-date">
+					<p class="resume-work-bold">
+						October 2016 - March 2016
+					</p>
+				</div>
 			</div>
 		</div>
-	</div> -->
+	</div>
+<!-- Anglo Eastern -->
+	<div class="row">
+		<div class="small-8 small-offset-2 columns">
+			<div class="callout warning" style="overflow: hidden;">
+				<div class="resume-work-company">
+					<h4 class="resume-work-bold">
+						Anglo Eastern Bloomingdale Building
+					</h4>
+					<p class="resume-work-para">IT Intern / On the Job training</p>
+				</div>
+				<div class="resume-work-date">
+					<p class="resume-work-bold">
+						April 2015 - May 2015
+					</p>
+				</div>
+			</div>
+		</div>
+	</div>
+<!-- OSM -->
+	<div class="row">
+		<div class="small-8 small-offset-2 columns">
+			<div class="callout warning" style="overflow: hidden;">
+				<div class="resume-work-company">
+					<h4 class="resume-work-bold">
+						OSM Maritime Group
+					</h4>
+					<p class="resume-work-para">IT Support Intern / On the Job training </p>
+				</div>
+				<div class="resume-work-date">
+					<p class="resume-work-bold">
+						April 2014 - June 2014
+					</p>
+				</div>
+			</div>
+		</div>
+	</div>
 </div>
 
 <?php include('foot.php'); ?>
